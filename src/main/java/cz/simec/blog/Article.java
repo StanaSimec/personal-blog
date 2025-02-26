d6f9059e41fc4eda29de5f6bccac615b7eab4bc6
standasimecek <simecek.stanis@gmail.com>
2025-02-26 13:13:11 +0100

    updated java to 21

--- src/main/java/cz/simec/blog/Article.java
+++ src/main/java/cz/simec/blog/Article.java
@@ -1,7 +1,6 @@
 package cz.simec.blog;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.time.Instant;
